0251445cd1d07c4127d7641ca710281591ff07a0
saadmumtaz <saadmumtaz619@gmail.com>
2025-08-02 17:11:43 +0500

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const express= require('express');
 const app=express();
 
-const middleware=(req,res,next)=>{
-  console.log("middleware execute");
-  next();
-};
-app.get("/",(req,res)=>{
-  res.send("hello saad");
-})
+app.set("view engine","ejs");
+app.use(express.static('./public'));
 
-app.get("/profile",(req,res)=>{
-  res.send('hello shayan');
-})
+app.get("/",function(req,res)
+{res.render("index",{age:12});
+});
+
+app.get("/error",function(req,res,next){
+    throw Error("something went wrong");
+});
+app.get("/contact",function(req,res)
+{res.render("contact",{name:harsh});
+});
+
+
+app.use(app.get('/', (req, res, next) => {
+  setTimeout(() => {
+    try {
+      throw new Error('BROKEN')
+    } catch (err) {
+      next(err)
+    }
+  }, 100)
+}))
 app.listen(3000);
