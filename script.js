7e726808bf4ed77099d40d263cc1c914db7ed69f
saadmumtaz <saadmumtaz619@gmail.com>
2025-08-07 20:53:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "banana", "cherry", "banana"];
+
+let index = fruits.indexOf("banana");
+
+console.log(index); // Output: 1 (pehli "banana" ka index)
+
+
+let arr=[1,2,3,4,5,6];
+let saas=arr.indexOf(4);
+console.log(saas);
