b8ce216deea2c6c6e80437667cd64f655eb602ed
saadmumtaz <saadmumtaz619@gmail.com>
2025-08-16 20:22:30 +0500

    this is a comment

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const fs = require("fs");
+fs.unlink("chacha.txt",function(err){
+    if (err)console.error(err);
+    else console.log("removed");
+})
 
-const filename = "saad.txt"
-
-try {
-    fs.rename(filename, "shayan.txt", function (err) {
-        if (err) console.error(err);
-        else console.log("newfile");
-    })
-
-    fs.rename("shayan.txt", "saad.txt")
-} catch (error) {
-   console.log("an error occur while managing file", error.message)
-//    cons
-}
