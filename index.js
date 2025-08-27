bad677c0080c05c46598df10aae189a38ecb0e97
saadmumtaz <saadmumtaz619@gmail.com>
2025-08-27 11:10:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const app = express();
+const path = require("path");
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set('view engine','ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render("index.ejs");
+});
+app.get('/profile/:username',(req,res)=>{
+    res.send(req.params.username);
+})
+app.listen(3000,function(){
+    console.log("its running");
+})
