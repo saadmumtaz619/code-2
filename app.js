8a95090973c71426051be866f63332d8b129b29b
saadmumtaz <saadmumtaz619@gmail.com>
2025-09-10 12:54:33 +0500

    first commit

--- app.js
+++ app.js
@@ -1,47 +1,16 @@
-const express= require('express');
-const app= express();
-const userModel =require("./usermodel")
+const express = require('express');
+const app = express();
+const path = require('path');
 
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use (express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
-    res.send("hello i am here")
+    res.render("index.ejs");
 })
-
-
-app.get('/create',async (req,res)=>{
-   let createduser= await userModel.create({
-        name:"harash",
-        email:"harash@gmail.com",
-        username:"harash"
-
-    })
-    res.send(createduser);
+app.get('/read',(req,res)=>{
+    res.render("read.ejs")
 })
-
-app.get('/create', async (req,res)=>{
-     let createduser = await userModel.create({
-        name:"harastia",
-        email:"harastia@gmail.com",
-        usrname:"harastia"
-    })
-    res.send(createduser);
-})
-
-
-app.get('/update',async (req,res)=>{
-let updateduser=awaituserModel.findoneAndupdate({username:"harash"},{name:"harsh vandana"} ,{new:true})
-res.send(createduser);
-})
-
-app.get('/read', async (req,res)=>{
-    let users= await userModel.find({username:"harash"});
-    res.send(users);
-})
-
-app.get('/delete', async (req,res)=>{
-    let users = await userModel.findone({username:"harash"});
-    res.send(users);
-})
-
-
 app.listen(3000);
